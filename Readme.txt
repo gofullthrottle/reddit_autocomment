89ee016bacac70d82d7adfb62638423a10be656a
Mark j <mjan8468@gmail.com>
2013-06-18 23:07:53 +1000

    v .1
    Rushed first version

---
+++ Readme.txt
@@ -0,0 +1,15 @@
+1. Download the following files if you don't have them already:
+	
+Python 2.7 - http://www.python.org/getit/ - Get the corresponding version to your os
+	
+Setup tools - https://pypi.python.org/pypi/setuptools/0.7.2
+	
+Praw - https://pypi.python.org/pypi/praw/2.0.1
+
+2. Install your version of python and take note to where it's being installed to
+
+3. Install setup tools. On windows this is as easy as running ez_setup.py which is located in the setuptools archive
+
+4. Install praw. On windows, open cmd, type cd C:\Python27\Scripts (Note: your directory may be elsewhere), type easy_install praw
+
+5. Run automated_commenter.py and use freely
---
+++ automated_commenter.py
@@ -0,0 +1,131 @@
+import praw, subprocess, time, tkMessageBox
+from Tkinter import *
+
+r = praw.Reddit(user_agent='commenter')
+
+def login(username, password):
+            r.login(username, password)
+
+class subreddit(object):
+    def __init__(self, name):
+        """a subreddit with given name
+
+        __init__ (string) 
+        """
+        self.sub = r.get_subreddit(name)
+        self.posts = None
+
+    def get_top(self):
+        """get top submissions of subreddit
+
+        """
+        self.posts = list(self.sub.get_top())
+
+    def get_new(self):
+        """get new submissions of subreddit
+
+        """
+        self.posts = list(self.sub.get_new())
+
+    def find_post(self, title):
+        """find the index of a submission with given title in
+        the subreddit
+
+        find_submission(str) -> int
+        """
+        for post in self.posts:
+            if title in post.title:
+                index = self.posts.index(post)
+                return self.posts[index]
+
+    def comment(self, post, text):
+        """comment text into submission
+
+        comment(str,str)
+        """
+        self.find_post(post).add_comment(text)
+
+class Controller(Frame):
+    
+    def __init__(self, master):
+        """Create app elements: Main frame, login options and 
+
+        Constructor:Controller(Tk)
+
+        """
+        self.master= master
+
+        """login"""        
+        loginframe = Frame(self.master)
+        Button(loginframe, text='Login',
+               command=self.login).pack(padx=10)
+        self.username = Entry(loginframe)
+        self.username.pack()
+        self.password = Entry(loginframe)
+        self.password.pack()
+        self.v = StringVar()
+        Label(loginframe, textvariable=self.v).pack()
+        self.x = IntVar()
+        chk = Checkbutton(loginframe, text='Shutdown after commented', variable=self.x)
+        chk.pack(side=LEFT)
+        loginframe.pack(side=LEFT)
+        """entry field"""
+        entryframe = Frame(self.master)
+        Label(entryframe, text='Subreddit').pack()
+        self.sub = Entry(entryframe)
+        self.sub.pack()
+        Label(entryframe, text='Post Title').pack()
+        self.post = Entry(entryframe)
+        self.post.pack()
+        entryframe.pack()
+        """text field"""
+        textframe = Frame(self.master)
+        Label(textframe, text='Comment').pack()
+        self.text = Text(textframe, width=30, height=20)
+        self.text.pack()
+        Button(textframe, text='Submit',
+               command=self.submit).pack()
+        textframe.pack()
+        Label(self.master, text='Created by Danejazone').pack(side=BOTTOM)
+
+
+    def login(self):
+        """login to reddit using username and password
+
+        """
+        if len(self.username.get()) > 0:
+            try:
+                r.login(self.username.get(),self.password.get())
+                self.v.set('Logged in')
+            except Exception:
+                self.v.set('Invalid username or password')
+            
+    def submit(self):
+            sub = subreddit(self.sub.get())
+            sub.get_new()
+            title = self.post.get()
+            text = self.text.get('1.0','end')
+            if r.is_logged_in() == False:
+                tkMessageBox.showinfo('Error','Not Logged in')
+                return
+            else:
+                while True:
+                    sub.comment(title,text)
+            if self.x.get() == 1:
+                subprocess.call(["shutdown.exe", "-f", "-s", "-t", "0"])
+            
+            
+class CommentApp():
+    def __init__(self, master=None):
+        master.title("Reddit AutoComment")
+        self.controller = Controller(master)
+
+def main():
+    root = Tk()
+    root.geometry("500x500")
+    root.resizable(width=FALSE, height=FALSE)
+    app = CommentApp(root)
+    root.mainloop()
+    
+if  __name__ == '__main__':
+    main()
